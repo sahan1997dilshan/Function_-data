9136a2aa4dba7f61eff25b2874f6aab2fdb7252d
sahan_dilshan <dsahan548@gmail.com>
2021-09-06 13:55:51 +0530

    Change transfer method

--- q3.scala
+++ q3.scala
@@ -9,12 +9,31 @@ object q3
 
    def deposit(a:Double) = this.balance+=a;
 
-   def transfer(a:Account,b:Double):Unit={
+   def transfer(that:Account,b:Double):Unit={
     this.balance=this.balance-b;
+    that.balance=that.balance+b;
      }
    
     override def toString =
      "["+nic+":"+acnumber +":"+balance+"]"
 }
+    def main(args: Array[String]):Unit = {
+      val acc1=new Account("110",20,1000)
+      val acc2=new Account("120",25,900)
+
+      println("Before Tranfer")
+      println(acc1)
+      println(acc2)
+       
+      acc1.transfer(acc2,100)
+      println()
+
+      println("After Tranfer")
+      println(acc1)
+      println(acc2)
+
+
+      
+    }
  
 }
